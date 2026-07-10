c028f021cc14251991f02885ccb2ecbd45fff965
Tony3243 <113482331+Tony3243@users.noreply.github.com>
2026-07-10 12:48:09 -0700

    Initial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,31 @@
 import 'dotenv/config';
 import express from 'express';
 import { pool } from './db/pool.js';
+import * as http from 'node:http'
+import {Server} from 'socket.io'
 
 // Fail fast if DATABASE_URL is unreachable — before accepting any traffic
 const client = await pool.connect();
 client.release();
 
 const app = express();
+const server = http.createServer(app) //wraps our existing app
+const io = new Server(server)//intergrate socket.io and implmented as an instance
 const port = process.env.PORT ?? 3000;
 
 app.get('/health', (_req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.listen(port, () => {
+//basic connection
+io.on('connection', (socket) => {
+  console.log("A user connected");
+  socket.on('disconnect', () => {
+    console.log("User disconnected")
+  })
+})
+
+
+server.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
