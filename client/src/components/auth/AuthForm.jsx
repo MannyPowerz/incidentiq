7b0454e76bd681efcd71b8db3867b7dddbd3a5a8
gabrielaiduarte <gabrielaidg03@gmail.com>
2026-07-22 16:59:09 -0400

    create authentication component structure

---
+++ client/src/components/auth/AuthForm.jsx
@@ -0,0 +1,11 @@
+import "./AuthForm.css"
+
+export default function AuthForm() {
+    // Authentication form structure
+    return (
+        <form className="auth-form">
+            <h1>Welcome back!</h1>
+            <p>Sign in to your account</p>
+        </form>
+    );
+}
---
+++ client/src/pages/SignInPage.jsx
@@ -0,0 +1,10 @@
+import AuthForm from "../components/auth/AuthForm";
+
+export default function SignInPage() {
+    // Display reusable authetication form
+    return (
+        <main className="sign-in-page">
+            <AuthForm />
+        </main>
+    );
+}
