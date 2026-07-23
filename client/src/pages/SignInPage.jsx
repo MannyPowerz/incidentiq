3237dda37d81ba55bada2f2a45389814ad04c4f1
gabrielaiduarte <gabrielaidg03@gmail.com>
2026-07-22 21:11:32 -0400

    add logo to sign in page

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -4,7 +4,28 @@ export default function SignInPage() {
     // Display reusable authetication form
     return (
         <main className="sign-in-page">
-            <AuthForm />
+            <div className="background-glow background-glow-left" />
+            <div className="background-glow background-glow-right"/>
+
+            <section className="sign-in-content">
+                {/* Display logo at the top */}
+                <header className="sign-in-brand">
+                    <div className="logo" aria-hidden="true">
+                        <span className="logo-center" />
+                    </div>
+
+                    <div className="logo-text">
+                        <p className="logo-name">
+                            Incident<span>IQ</span>
+                        </p>
+                        <p className="logo-tagline">
+                            Detect. Collaborate. Resolve.
+                        </p>
+                    </div>
+                </header>
+
+                <AuthForm />
+            </section>
         </main>
     );
 }
