4eadc308a649a4859b341baef76ecccd1acefb32
gabrielaiduarte <gabrielaidg03@gmail.com>
2026-07-27 15:52:32 -0400

    refactor: add typescript types to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
+import type { JSX } from "react";
 import { Navigate, Route, Routes } from "react-router-dom"
 import SignInPage from "./pages/SignInPage";
 import DashboardPage from "./pages/DashboardPage";
 
-export default function App() {
+export default function App() : JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/signin" replace />} />
