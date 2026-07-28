0522e669ab142e2d04440016e7d609aed00515ef
gabrielaiduarte <gabrielaidg03@gmail.com>
2026-07-28 15:37:11 -0400

    add rooms page shell and route

---
+++ client/src/pages/RoomsPage.tsx
@@ -0,0 +1,10 @@
+import type { JSX } from "react";
+import "./RoomsPage.css"
+
+export default function RoomsPage () : JSX.Element {
+    return (
+        <main className="rooms-page">
+            
+        </main>
+    )
+}
