287412c25ae5641eece8fc83d57aa038e2616437
gabrielaiduarte <gabrielaidg03@gmail.com>
2026-08-05 10:39:07 -0400

    Set up room details page structure

---
+++ client/src/pages/RoomDetailsPage.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "react";
+
+export default function RoomDetailsPage () : JSX.Element {
+    return (
+        <div>
+            <h1>Incident Room</h1>
+        </div>
+    )
+}
